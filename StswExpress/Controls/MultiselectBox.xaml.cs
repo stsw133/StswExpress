0dae0c6396da8cb0c6750c441ecfbfe75dac69ba
Karol.Staszewski <karol.staszewski@daw-systems.pl>
2021-04-26 12:11:56 +0200

    1. Prototyp nowej kontrolki: MultiselectBox 2. Zredukowanie pliku logów do minimum

---
+++ StswExpress/Controls/MultiselectBox.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace StswExpress.Controls
+{
+    /// <summary>
+    /// Interaction logic for MultiselectBox.xaml
+    /// </summary>
+    public partial class MultiselectBox : ComboBox
+    {
+        PropertyInfo prop;
+
+        public MultiselectBox()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            prop = GetType().GetProperty(nameof(SelectionBoxItem));
+            prop = prop.DeclaringType.GetProperty(nameof(SelectionBoxItem));
+        }
+
+        /// <summary>
+        /// Source
+        /// </summary>
+        public object[] Source
+        {
+            get => (object[])GetValue(pSource);
+            set
+            {
+                SetValue(pSource, value);
+                Items.Clear();
+                for (int i = 0; i < Source.Length; i++)
+                {
+                    var tbtn = new ToggleButton()
+                    {
+                        Background = Brushes.Transparent,
+                        BorderThickness = new Thickness(0),
+                        Content = Source[i]?.ToString()
+                    };
+                    tbtn.Click += ToggleButton_Click;
+                    Items.Add(tbtn);
+                }
+            }
+        }
+        public static readonly DependencyProperty pSource
+            = DependencyProperty.Register(
+                  nameof(Source),
+                  typeof(object[]),
+                  typeof(MultiselectBox),
+                  new PropertyMetadata(null)
+              );
+
+        /// <summary>
+        /// SelectedIndexes
+        /// </summary>
+        public int[] SelectedIndexes
+        {
+            set
+            {
+                SetValue(pSelectedIndexes, value);
+                for (int i = 0; i < Items.Count; i++)
+                {
+                    if (value.Contains(i))
+                        (Items[i] as ToggleButton).IsChecked = true;
+                    else
+                        (Items[i] as ToggleButton).IsChecked = false;
+                }
+                ToggleButton_Click(null, null);
+            }
+        }
+        public static readonly DependencyProperty pSelectedIndexes
+            = DependencyProperty.Register(
+                  nameof(SelectedIndexes),
+                  typeof(int[]),
+                  typeof(MultiselectBox),
+                  new PropertyMetadata(null)
+              );
+
+        /// <summary>
+        /// SelectedContents
+        /// </summary>
+        public object[] SelectedContents
+        {
+            get
+            {
+                var result = new List<object>();
+                foreach (ToggleButton item in Items)
+                    if (item.IsChecked == true)
+                        result.Add(item.Content);
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Click
+        /// </summary>
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            prop.SetValue(this, string.Join("; ", SelectedContents), BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
+        }
+    }
+}
--- StswExpress/Globals/Log.cs
+++ StswExpress/Globals/Log.cs
@@ -2,184 +2,51 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace StswExpress.Globals
 {
     public static class Log
     {
+        private static string LogFileName = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), $"log.log");
+        private static FileInfo LogFile = new FileInfo(LogFileName);
+        public static long ArchiveFileSize = 1024 * 1024 * 5; /// 5 MB
+        
         /// <summary>
-        /// Plik tekstowy do wstawiania komunikatów.
+        /// Write text to log file.
         /// </summary>
-        private static FileInfo logFile;
-        /// <summary>
-        /// Data utworzenia pliku Log
-        /// </summary>
-        private static DateTime LogFileCreateTime = DateTime.Today;
-        /// <summary>
-        /// Nazwa pliku tekstowego logu.
-        /// </summary>
-        private static string LogFileName = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), $"log {LogFileCreateTime:yyyy-MM-dd)}.log");
-        /// <summary>
-        /// Długość pliku przeznaczonego do archiwizacji.
-        /// </summary>
-        private const long ArchivizationFileLenght = 20 * 1024 * 1024; /// 20 MB
-        /// <summary>
-        /// Kodowanie pliku tektowego logu.
-        /// </summary>
-        private static Encoding ansi = Encoding.GetEncoding("windows-1250");
-        /// <summary>
-        /// Piska tekstowy do pliku.
-        /// </summary>
-        private static StreamWriter strWr;
-        /// <summary>
-        /// Wymuszanie pokazywania nazw funkcji wywołujących w komunikatach logów
-        /// </summary>
-        private const bool EnforceShowCallerFunction = true;
-        /// <summary>
-        /// Obecny proces - do wyciągania informacji o pamięci
-        /// </summary>
-        private static Process process;
-
-        /// <summary>
-        /// Inicjalizuje log w postaci pliku tekstowego.
-        /// </summary>
-        /// <returns>Kod błędu lub 0 gdy poprawny.</returns>
-        private static void InitializeTextFileLog()
-        {
-            logFile = new FileInfo(LogFileName);
-            if (logFile != null)
-            {
-                /// Tworzymy wpis informacji startowej              
-                WriteTextFileEntry($"{DateTime.Now:yyyy-MM-dd | HH:mm:ss} |   v{Global.AppVersion()}{Environment.NewLine}");
-            }
-        }
-
-        /// <summary>
-        /// Archiwizuje plik logu jeżeli przekracza on zadaną wielkość.
-        /// </summary>
-        /// <returns>Kod błędu lub 0 gdy poprawny.</returns>
-        public static void FileLogArchivization()
-        {
-            if (File.Exists(LogFileName))
-            {
-                if (logFile.Length > ArchivizationFileLenght)
-                {
-                    File.Copy(LogFileName, LogFileName.Replace(".", $"_archive_{DateTime.Now:yyyyMMddhhmmss}."));
-                    File.WriteAllText(LogFileName, string.Empty, ansi);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Wpisuje zadany tekst do plik tekstowego.
-        /// </summary>
-        /// <param name="text">Zadany tekst.</param>
-        /// <returns>Kod błędu lub 0 gdy poprawny.</returns>
-        private static void WriteTextFileEntry(string text)
+        /// <param name="text">Text.</param>
+        /// <param name="addfunccallername">Add function name.</param>
+        public static void WriteLogEntry(string text, bool addfunccallername = false)
         {
             try
             {
-                FileLogArchivization();
-                FileLogDateCheck();
-
-                /// Tworzymy nowy pisak do pliku, zapisujemy informację i zamykamy pisak
-                using (strWr = new StreamWriter(LogFileName, true, ansi))
+                /// add func name
+                if (addfunccallername)
                 {
-                    strWr.Write(text);
+                    var stackTrace = new StackTrace();
+                    text += $" ({stackTrace.GetFrame(2).GetMethod().Name})";
+                }
+
+                /// make archive
+                if (File.Exists(LogFileName))
+                {
+                    if (LogFile.Length > ArchiveFileSize)
+                    {
+                        File.Copy(LogFileName, LogFileName.Replace(".", $"_archive_{DateTime.Now:yyyyMMdd_hhmmss}."));
+                        File.WriteAllText(LogFileName, string.Empty);
+                    }
+                }
+
+                /// write log
+                using (var strWr = new StreamWriter(LogFileName, true))
+                {
+                    strWr.Write($"{DateTime.Now:yyyy-MM-dd | HH:mm:ss.fff} | {text}{Environment.NewLine}");
                 }
             }
-            catch { }
-        }
-
-        public static void FileLogDateCheck()
-        {
-            if (File.Exists(LogFileName))
+            catch (Exception ex)
             {
-                DateTime today = DateTime.Today;
-                if (today > LogFileCreateTime)
-                {
-                    LogFileCreateTime = today;
-                    LogFileName = Path.Combine(Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath), $"log {LogFileCreateTime:yyyy-MM-dd}.log");
-                    WriteTextFileEntry($"{DateTime.Now:yyyy-MM-dd | HH:mm:ss} |   v{Global.AppVersion()}{Environment.NewLine}");
-                    WriteTextFileEntry($"{DateTime.Now:yyyy-MM-dd | HH:mm:ss} | Kontynuacja logu z dnia poprzedniego{Environment.NewLine}");
-                }
+                Console.WriteLine(ex.ToString());
             }
-        }
-
-        /// <summary>
-        /// Inicjalizuje logi programu.
-        /// </summary>
-        /// <param name="tb">Tekstowe okno logu.</param>
-        /// <returns>Kod błędu lub 0 gdy poprawny.</returns>
-        public static void InitializeLogs()
-        {
-            process = Process.GetCurrentProcess();
-            InitializeTextFileLog();
-        }
-
-        /// <summary>
-        /// Wstawia informację końcową i zamyka logowanie.
-        /// </summary>
-        /// <returns>Kod błędu lub 0 gdy poprawny.</returns>
-        public static void CloseLogs()
-        {
-            WriteTextFileEntry($"{DateTime.Now:yyyy-MM-dd | HH:mm:ss} | Koniec pracy.{Environment.NewLine}");
-            process.Dispose();
-        }
-
-        /// <summary>
-        /// Wpisuje zadany text do logów.
-        /// </summary>
-        /// <param name="text">Zadany tekst.</param>
-        /// <returns>Kod błędu lub 0 gdu poprawny.</returns>
-        public static void WriteLogEntry(string text)
-        {
-            WriteLogEntry(text, 0, false);
-        }
-
-        /// <summary>
-        /// Wpisuje zadany tekst wraz z informcją o funkcji do wszystkich logów.
-        /// </summary>
-        /// <param name="text">Zadany tekst.</param>
-        /// <param name="addfunccallername">Czy dodawać informację o funkcji.</param>
-        /// <returns>Kod błędu lub 0 jeżeli poprawny.</returns>
-        public static void WriteLogEntry(string text, bool addfunccallername)
-        {
-            WriteLogEntry(text, 0, addfunccallername);
-        }
-
-        /// <summary>
-        /// Wpisuje zadany tekst do wybranych logów.
-        /// </summary>
-        /// <param name="text">Zadany tekst.</param>
-        /// <param name="log">Wybrany log. 1 - pole tekstowe logu, 2- plik logu, inna liczba - wszystkie logi.</param>
-        /// <returns>Kod błedu lub 0 gdy poprawny.</returns>
-        public static void WriteLogEntry(string text, int log)
-        {
-            WriteLogEntry(text, log, false);
-        }
-
-        /// <summary>
-        /// Wpisuje zadany tekst wraz z informcją o funkcji do wybranego logu.
-        /// </summary>
-        /// <param name="text">Zadany tekst.</param>
-        /// <param name="log">Wybrny log. 1 - pole tekstowe logu, 2- plik logu, inna liczba - wszystkie logi.</param>
-        /// <param name="addfunccallername">Czy dodawać informację o funkcji.</param>
-        /// <returns>od błędu lub 0 gdy poprawny.</returns>
-        public static void WriteLogEntry(string text, int log, bool addfunccallername)
-        {
-            /// Dodaje informację o funkcji wywołuącej wstawiany komunikat
-            if (addfunccallername == true || EnforceShowCallerFunction == true)
-            {
-                StackTrace stackTrace = new StackTrace();
-                /// Pobieramy nazwę przedostatniej funkcji bo ostatnia jest zawsze przeciążoną funkcją WriteLogEntry
-                text += $" ({stackTrace.GetFrame(2).GetMethod().Name})";
-            }
-
-            /// Tworzy tekst wstawiany do logu
-            string entrytext = $"{DateTime.Now:yyyy-MM-dd | HH:mm:ss.fff} | {text}{Environment.NewLine}";
-            WriteTextFileEntry(entrytext);
         }
     }
 }
