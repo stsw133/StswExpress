ae6b2aa45140f988bb675a1aa42285097bd07ea8
Karol Staszewski <karol.staszewski@daw-systems.pl>
2023-05-12 16:04:09 +0200

    Prototype for StswButtonMenu control (have XAML binding failures but works - after resolving this control should be ready to use).

---
+++ StswExpress/Controls/StswButtonMenu.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace StswExpress;
+
+[ContentProperty(nameof(Buttons))]
+public class StswButtonMenu : ToggleButton
+{
+    public StswButtonMenu()
+    {
+        SetValue(ButtonsProperty, new ObservableCollection<ButtonBase>());
+    }
+    static StswButtonMenu()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(StswButtonMenu), new FrameworkPropertyMetadata(typeof(StswButtonMenu)));
+    }
+
+    #region Properties
+    /// Buttons
+    public static readonly DependencyProperty ButtonsProperty
+        = DependencyProperty.Register(
+            nameof(Buttons),
+            typeof(ObservableCollection<ButtonBase>),
+            typeof(StswButtonMenu)
+        );
+    public ObservableCollection<ButtonBase> Buttons
+    {
+        get => (ObservableCollection<ButtonBase>)GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
+    }
+
+    /// Header
+    public static readonly DependencyProperty HeaderProperty
+        = DependencyProperty.Register(
+            nameof(Header),
+            typeof(object),
+            typeof(StswButtonMenu)
+        );
+    public object? Header
+    {
+        get => (object?)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    #endregion
+
+    #region Style
+    /// > Background ...
+    /// BackgroundDisabled
+    public static readonly DependencyProperty BackgroundDisabledProperty
+        = DependencyProperty.Register(
+            nameof(BackgroundDisabled),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BackgroundDisabled
+    {
+        get => (Brush)GetValue(BackgroundDisabledProperty);
+        set => SetValue(BackgroundDisabledProperty, value);
+    }
+    /// BackgroundMouseOver
+    public static readonly DependencyProperty BackgroundMouseOverProperty
+        = DependencyProperty.Register(
+            nameof(BackgroundMouseOver),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BackgroundMouseOver
+    {
+        get => (Brush)GetValue(BackgroundMouseOverProperty);
+        set => SetValue(BackgroundMouseOverProperty, value);
+    }
+    /// BackgroundPressed
+    public static readonly DependencyProperty BackgroundPressedProperty
+        = DependencyProperty.Register(
+            nameof(BackgroundPressed),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BackgroundPressed
+    {
+        get => (Brush)GetValue(BackgroundPressedProperty);
+        set => SetValue(BackgroundPressedProperty, value);
+    }
+
+    /// > BorderBrush ...
+    /// BorderBrushDisabled
+    public static readonly DependencyProperty BorderBrushDisabledProperty
+        = DependencyProperty.Register(
+            nameof(BorderBrushDisabled),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BorderBrushDisabled
+    {
+        get => (Brush)GetValue(BorderBrushDisabledProperty);
+        set => SetValue(BorderBrushDisabledProperty, value);
+    }
+    /// BorderBrushMouseOver
+    public static readonly DependencyProperty BorderBrushMouseOverProperty
+        = DependencyProperty.Register(
+            nameof(BorderBrushMouseOver),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BorderBrushMouseOver
+    {
+        get => (Brush)GetValue(BorderBrushMouseOverProperty);
+        set => SetValue(BorderBrushMouseOverProperty, value);
+    }
+    /// BorderBrushPressed
+    public static readonly DependencyProperty BorderBrushPressedProperty
+        = DependencyProperty.Register(
+            nameof(BorderBrushPressed),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BorderBrushPressed
+    {
+        get => (Brush)GetValue(BorderBrushPressedProperty);
+        set => SetValue(BorderBrushPressedProperty, value);
+    }
+    /// BorderBrushDefaulted
+    public static readonly DependencyProperty BorderBrushDefaultedProperty
+        = DependencyProperty.Register(
+            nameof(BorderBrushDefaulted),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush BorderBrushDefaulted
+    {
+        get => (Brush)GetValue(BorderBrushDefaultedProperty);
+        set => SetValue(BorderBrushDefaultedProperty, value);
+    }
+
+    /// > Foreground ...
+    /// ForegroundDisabled
+    public static readonly DependencyProperty ForegroundDisabledProperty
+        = DependencyProperty.Register(
+            nameof(ForegroundDisabled),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush ForegroundDisabled
+    {
+        get => (Brush)GetValue(ForegroundDisabledProperty);
+        set => SetValue(ForegroundDisabledProperty, value);
+    }
+    /// ForegroundMouseOver
+    public static readonly DependencyProperty ForegroundMouseOverProperty
+        = DependencyProperty.Register(
+            nameof(ForegroundMouseOver),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush ForegroundMouseOver
+    {
+        get => (Brush)GetValue(ForegroundMouseOverProperty);
+        set => SetValue(ForegroundMouseOverProperty, value);
+    }
+    /// ForegroundPressed
+    public static readonly DependencyProperty ForegroundPressedProperty
+        = DependencyProperty.Register(
+            nameof(ForegroundPressed),
+            typeof(Brush),
+            typeof(StswButtonMenu)
+        );
+    public Brush ForegroundPressed
+    {
+        get => (Brush)GetValue(ForegroundPressedProperty);
+        set => SetValue(ForegroundPressedProperty, value);
+    }
+
+    /// > BorderThickness ...
+    /// PopupBorderThickness
+    public static readonly DependencyProperty PopupBorderThicknessProperty
+        = DependencyProperty.Register(
+            nameof(PopupBorderThickness),
+            typeof(Thickness),
+            typeof(StswButtonMenu)
+        );
+    public Thickness PopupBorderThickness
+    {
+        get => (Thickness)GetValue(PopupBorderThicknessProperty);
+        set => SetValue(PopupBorderThicknessProperty, value);
+    }
+
+    /// > CornerRadius ...
+    /// CornerRadius
+    public static readonly DependencyProperty CornerRadiusProperty
+        = DependencyProperty.Register(
+            nameof(CornerRadius),
+            typeof(CornerRadius),
+            typeof(StswButtonMenu)
+        );
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+    /// PopupCornerRadius
+    public static readonly DependencyProperty PopupCornerRadiusProperty
+        = DependencyProperty.Register(
+            nameof(PopupCornerRadius),
+            typeof(CornerRadius),
+            typeof(StswButtonMenu)
+        );
+    public CornerRadius PopupCornerRadius
+    {
+        get => (CornerRadius)GetValue(PopupCornerRadiusProperty);
+        set => SetValue(PopupCornerRadiusProperty, value);
+    }
+
+    /// > Padding ...
+    /// PopupPadding
+    public static readonly DependencyProperty PopupPaddingProperty
+        = DependencyProperty.Register(
+            nameof(PopupPadding),
+            typeof(Thickness),
+            typeof(StswButtonMenu)
+        );
+    public Thickness PopupPadding
+    {
+        get => (Thickness)GetValue(PopupPaddingProperty);
+        set => SetValue(PopupPaddingProperty, value);
+    }
+    #endregion
+}
---
+++ TestApp/Modules/Controls/StswButtonMenu/StswButtonMenuView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace TestApp;
+/// <summary>
+/// Interaction logic for StswButtonMenuView.xaml
+/// </summary>
+public partial class StswButtonMenuView : UserControl
+{
+    public StswButtonMenuView()
+    {
+        InitializeComponent();
+    }
+}
