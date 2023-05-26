1fbd436a2a31ee4e054580d105e8b354fdb97dfd
Karol Staszewski <karol.staszewski@daw-systems.pl>
2023-05-26 16:23:30 +0200

    StswColorBox extended by adding color to select. In the future this will be propably a new control.

--- TestApp/Modules/Controls/StswColorPicker/StswColorPickerContext.cs
+++ TestApp/Modules/Controls/StswColorPicker/StswColorPickerContext.cs
@@ -1,6 +1,13 @@
-﻿namespace TestApp;
+﻿using System.Windows.Media;
+
+namespace TestApp;
 
 public class StswColorPickerContext : StswObservableObject
 {
-    
+    private Color color = Color.FromRgb(255, 0, 0);
+    public Color Color
+    {
+        get => color;
+        set => SetProperty(ref color, value);
+    }
 }
