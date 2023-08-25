fc13f996bd75ae5bc78ae59f45b73f08885d1223
Karol Staszewski <karol.staszewski@daw-systems.pl>
2023-08-25 15:46:11 +0200

    All Stsw controls in read only state have some opacity on content presenter.

--- TestApp/Modules/Home/HomeContext.cs
+++ TestApp/Modules/Home/HomeContext.cs
@@ -1,6 +1,11 @@
-﻿namespace TestApp;
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace TestApp;
 
 public class HomeContext : StswObservableObject
 {
-    
+    public string? Author => FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).CompanyName;
+    public string? Name => Assembly.GetExecutingAssembly().GetName().Name;
+    public string? Version => $"{Assembly.GetExecutingAssembly().GetName().Version?.Major}.{Assembly.GetExecutingAssembly().GetName().Version?.Minor}";
 }
