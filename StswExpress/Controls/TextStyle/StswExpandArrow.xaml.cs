e0aa56ed5f89c3d6fcd733d2d6b57fa804efa5aa
Hubert <hubert.rogowski@daw-systems.pl>
2023-11-30 13:35:39 +0100

    expand arrow added

---
+++ StswExpress/Controls/TextStyle/StswExpandArrow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StswExpress;
+
+public class StswExpandArrow : Control
+{
+    static StswExpandArrow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(StswExpandArrow), new FrameworkPropertyMetadata(typeof(StswExpandArrow)));
+    }
+
+    public bool IsExpanded
+    {
+        get { return (bool)GetValue(IsExpandedProperty); }
+        set { SetValue(IsExpandedProperty, value); }
+    }
+    public static readonly DependencyProperty IsExpandedProperty =
+        DependencyProperty.Register(
+            nameof(IsExpanded),
+            typeof(bool), 
+            typeof(StswExpandArrow), 
+            new PropertyMetadata(false));
+
+
+}
