cba6d37c816f9977c3241ba14e88d5f53f380bcd
stsw <stsw.karolstaszewski@gmail.com>
2024-01-24 22:19:57 +0100

    1. New converter: StswIsTypeConverter. 2. StswText and StswOutlinedText have different disabled style. StswHeader and StswLabel check if content is StswText type to avoid doubling disabled style.

---
+++ StswExpress/Utils/Converters/Bool/StswIsTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace StswExpress;
+
+/// <summary>
+/// Compares value type to type in parameter.<br/>
+/// </summary>
+public class StswIsTypeConverter : MarkupExtension, IValueConverter
+{
+    private static StswIsTypeConverter? instance;
+    public static StswIsTypeConverter Instance => instance ??= new StswIsTypeConverter();
+    public override object ProvideValue(IServiceProvider serviceProvider) => Instance;
+
+    /// Convert
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => parameter is Type type ? value?.GetType() == type : Binding.DoNothing;
+
+    /// ConvertBack
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => Binding.DoNothing;
+}
