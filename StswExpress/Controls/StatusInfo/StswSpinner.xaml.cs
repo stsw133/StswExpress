d0fcede0eadc032a5f7e56e899187f90ced5f931
Hubert <hubert.rogowski@daw-systems.pl>
2024-04-17 13:37:41 +0200

    dodanie spinnera

---
+++ StswExpress/Controls/StatusInfo/StswSpinner.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StswExpress;
+
+public class StswSpinner : Control
+{
+    #region Constructor
+    static StswSpinner()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(StswSpinner), new FrameworkPropertyMetadata(typeof(StswSpinner)));
+    }
+    #endregion
+
+    #region DP
+    public SpinnerType Type
+    {
+        get { return (SpinnerType)GetValue(TypeProperty); }
+        set { SetValue(TypeProperty, value); }
+    }
+    public static readonly DependencyProperty TypeProperty =
+        DependencyProperty.Register(nameof(Type), typeof(SpinnerType), typeof(StswSpinner), new PropertyMetadata(SpinnerType.Circle1));
+    #endregion
+}
+
+public enum SpinnerType
+{
+    Circle1,
+    Circle2,
+    Dots,
+}
