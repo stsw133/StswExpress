3f7b7791488cfdf9152c1e29d21d5f109f64792e
Hubert <hubert.rogowski@daw-systems.pl>
2025-01-22 14:15:15 +0100

    reorderlist

---
+++ TestApp/Modules/Controls/Selectors/StswReorderBox/StswReorderListView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace TestApp;
+
+/// <summary>
+/// Interaction logic for StswReorderListView.xaml
+/// </summary>
+public partial class StswReorderListView : UserControl
+{
+    public StswReorderListView()
+    {
+        InitializeComponent();
+    }
+}
